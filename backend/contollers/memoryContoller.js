928635f938f7106414909d183d2e425949bdc088
MeGallin <me@garyallin.uk>
2022-02-15 15:30:49 +0000

    Middleware added

--- backend/contollers/memoryContoller.js
+++ backend/contollers/memoryContoller.js
@@ -1,26 +1,33 @@
+import asyncHandler from 'express-async-handler';
+
 // @description: Get All the Memories
 // @route: GET /api/memories
 // @access: Public
-const getAllMemories = (req, res) => {
+const getAllMemories = asyncHandler(async (req, res) => {
   res.status(200).json({ memories: 'All memories listed here' });
-};
+});
 // @description: Create a Memory
 // @route: POST /api/memories
 // @access: Private
-const createMemory = (req, res) => {
-  res.status(200).json({ Memory: 'New Memory created now' });
-};
+const createMemory = asyncHandler(async (req, res) => {
+  if (!req.body.memory) {
+    res.status(400);
+    throw new Error('No message included');
+  } else {
+    res.status(200).json({ Memory: 'New Memory created now' });
+  }
+});
 // @description: Update a Memory
 // @route: PUT /api/memory/:id
 // @access: Private
-const updateMemory = (req, res) => {
+const updateMemory = asyncHandler(async (req, res) => {
   res.status(200).json({ Memory: `Update memory ${req.params.id}` });
-};
+});
 // @description: Update a Memory
 // @route: PUT /api/memory/:id
 // @access: Private
-const deleteMemory = (req, res) => {
+const deleteMemory = asyncHandler(async (req, res) => {
   res.status(200).json({ Memory: `Memory ${req.params.id} deleted` });
-};
+});
 
 export { getAllMemories, createMemory, updateMemory, deleteMemory };
