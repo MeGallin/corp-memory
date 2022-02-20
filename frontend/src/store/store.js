76755bab18ba2e1100e3d9d4ae348bc2db5a4edb
MeGallin <me@garyallin.uk>
2022-02-20 10:39:56 +0000

    logi working

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,13 +2,24 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import { userRegistrationReducer } from './reducers/userReducers';
+import {
+  userRegistrationReducer,
+  userLoginReducer,
+} from './reducers/userReducers';
+
+//Initialise state to hold user info if logged in.
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null;
 
 const reducer = combineReducers({
   userRegistration: userRegistrationReducer,
+  userLogin: userLoginReducer,
 });
 
-const initialState = {};
+const initialState = {
+  userLogin: { userInfo: userInfoFromStorage },
+};
 const middleware = [thunk];
 
 const store = createStore(
