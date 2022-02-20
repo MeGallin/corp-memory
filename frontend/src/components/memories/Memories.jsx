ea08d6923ac449f07e233a2428cb40933e5b8ef4
MeGallin <me@garyallin.uk>
2022-02-20 18:52:07 +0000

    minor tidy up

--- frontend/src/components/memories/Memories.jsx
+++ frontend/src/components/memories/Memories.jsx
@@ -21,12 +21,10 @@ const Memories = () => {
   }, [userInfo, dispatch]);
 
   const userMemories = useSelector((state) => state.userMemories);
-  const { loading, success, error, memories } = userMemories;
+  const { loading, memories } = userMemories;
 
   return (
     <div className="memories-wrapper">
-      {error ? error : null}
-      {success ? 'Registration has been successful' : null}
       {loading ? (
         'loading'
       ) : (
@@ -36,7 +34,6 @@ const Memories = () => {
               <div>
                 <fieldset className="fieldSet">
                   <legend>Memories</legend>
-
                   {memories?.map((memory) => (
                     <div key={memory._id}>
                       <p>{memory._id}</p>
@@ -44,7 +41,7 @@ const Memories = () => {
                       <p>{memory.rating}</p>
                       <DeleteMemory id={memory._id} />
                       <UpdateMemory updateMemory={{ ...memory }} />
-                      <hr />
+                      <hr className="style-one" />
                     </div>
                   ))}
                 </fieldset>
@@ -54,7 +51,47 @@ const Memories = () => {
             </div>
           ) : (
             <>
-              <h1>Home page data</h1>
+              <h1>Corporate memory</h1>
+              <p>
+                Corporate memory is the ability of an organization to retain
+                information to improve strategy, decision making, problem
+                solving, operations and design. An organization with low
+                corporate memory is doomed to repeat the same mistakes and
+                reinvent things repeatedly in a costly loop. The following are
+                the basic components of corporate memory.
+              </p>
+              <h2>Knowledge</h2>
+              <p>
+                The abilities and knowledge of your employees. Generally
+                speaking, when people leave corporate memory is lost. That is to
+                say that there is always information that isn't transfered or
+                retained such as tacit knowledge and situational knowledge. For
+                example, a salesperson who has figured out an effective method
+                for selling to a particular executive might be unlikely to
+                communicate the approach to someone who could replicate it.
+              </p>
+              <h2>Information</h2>
+              <p>
+                Data designed to be consumed by people. For example, a design
+                document or a training video. It is common for knowledge workers
+                to produce copious amounts of documentation that is archived in
+                a tool such as a knowledge management platform. It is also
+                common for such information to go to waste or for similar
+                documentation efforts to be repeated many times.
+              </p>
+              <h2>Data</h2>
+              <p>
+                Information designed to be consumed by machine. Automation and
+                decision support based on databases is a type of corporate
+                memory that survives employee turnover. In some cases, replacing
+                systems and changing processes results in data going dark.
+              </p>
+              <h2>Organizational Culture</h2>
+              <p>
+                The norms, habits and expectations of a firm. As with the
+                culture of a nation, this is rooted in history and serves as a
+                stabilizing force that doesn't easily change.
+              </p>
             </>
           )}
         </>
