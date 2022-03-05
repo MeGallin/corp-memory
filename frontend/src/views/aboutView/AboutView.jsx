395a53c5e2034481a12bf382c09e89001c3c4440
MeGallin <me@garyallin.uk>
2022-03-05 13:02:39 +0000

    basic setup

--- frontend/src/views/aboutView/AboutView.jsx
+++ frontend/src/views/aboutView/AboutView.jsx
@@ -1,7 +1,25 @@
 import './AboutView.scss';
 
 const About = () => {
-  return <div className="about-wrapper">About</div>;
+  return (
+    <div className="about-wrapper">
+      <h1>About Us</h1>
+      <p>
+        YCM, is a simple to use productivity tool that can be used to save,
+        retrieve and edit information in a secure environment.
+      </p>
+      <p>
+        Simply, create an account confirm you email address and log into your
+        secure dashboard.
+      </p>
+      <p>Create a new memory, add a an optional tag and set a reminder.</p>
+      <p>There is no limit to the number of memories.</p>
+      <h1>The Tech</h1>
+      <p>Build using the MERN stack </p>
+      <h1>Contact</h1>
+      <p>Feel free to contact us via our contact form.</p>
+    </div>
+  );
 };
 
 export default About;
