abae90f24becd61dc39ea2adb5ed7591ea66700d
MeGallin <me@garyallin.uk>
2022-04-06 10:25:57 +0100

    minor changes

--- frontend/src/components/adminUserMemories/AdminUsersMemories.jsx
+++ frontend/src/components/adminUserMemories/AdminUsersMemories.jsx
@@ -9,12 +9,12 @@ import moment from 'moment';
 const AdminUsersMemories = () => {
   const dispatch = useDispatch();
 
+  const adminUserMemories = useSelector((state) => state.adminUserMemories);
+  const { loading, success, error, usersMemories } = adminUserMemories;
+
   useEffect(() => {
     dispatch(adminUserMemoriesAction());
   }, [dispatch]);
-
-  const adminUserMemories = useSelector((state) => state.adminUserMemories);
-  const { loading, success, error, usersMemories } = adminUserMemories;
 
   const memories = usersMemories?.filter((memory) => {
     if (memory.user) {
@@ -38,27 +38,33 @@ const AdminUsersMemories = () => {
         <>
           {users?.map((user) => (
             <div key={user?._id} className="inner-wrapper">
-              {!user.isAdmin ? (
-                <>
-                  <h2>{user?.name}</h2>
-                  <p>{user?.email}</p>
-                  <p>{user?.isConfirmed}</p>
-                  <p>{user?.isAdmin}</p>
-                  <p>
-                    Created:
-                    {moment(user?.createdAt).startOf('minute').fromNow()}
-                  </p>
-                  <p>
-                    Updated:
-                    {moment(user?.updatedAt).startOf('minute').fromNow()}
-                  </p>
-                </>
-              ) : null}
+              <>
+                <h2>{user?.name}</h2>
+                <p>
+                  Email:{' '}
+                  <a href={`mailto:${user?.email}`} target="_blank">
+                    {user?.name}
+                  </a>{' '}
+                  [{user?.email}]
+                </p>
+                <p>{user?.isConfirmed}</p>
+                <p>{user?.isAdmin}</p>
+                <p>
+                  Created: {moment(user?.createdAt).startOf('minute').fromNow()}
+                </p>
+                <p>
+                  Updated: {moment(user?.updatedAt).startOf('minute').fromNow()}
+                </p>
+              </>
 
               {memories?.map((memory) => (
                 <div key={memory?._id}>
                   {user?._id === memory?.user ? (
-                    <div>{memory?.title}</div>
+                    <>
+                      {console.log(memory)}
+                      <h3>{memory?.title}</h3>
+                      <p>{memory.memory}</p>
+                    </>
                   ) : null}
                 </div>
               ))}
