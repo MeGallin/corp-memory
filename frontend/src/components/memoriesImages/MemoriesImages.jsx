5d7629033d38b3b68c608d9148aac0fccaba5bc2
MeGallin <me@garyallin.uk>
2022-08-23 09:27:41 +0100

    upload image working

---
+++ frontend/src/components/memoriesImages/MemoriesImages.jsx
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import './MemoriesImages.scss';
+
+import { memoryImageUploadAction } from '../../store/actions/imageUploadAction';
+
+import InputFieldComponent from '../inputField/inputFieldComponent';
+
+const MemoriesImages = ({ memoryId, memoryImage }) => {
+  const dispatch = useDispatch();
+  const [previewImage, setPreviewImage] = useState('');
+  const [previewImageFile, setPreviewImageFile] = useState('');
+
+  const previewFile = (file) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setPreviewImage(reader.result);
+    };
+  };
+
+  const uploadFileHandler = (e) => {
+    const imageFile = e.target.files[0];
+    setPreviewImageFile(imageFile);
+    previewFile(imageFile);
+  };
+
+  const handelCancelUpload = () => {
+    document.getElementById('memoryImage').value = '';
+    setPreviewImage('');
+  };
+
+  const handleImageUpdate = (e) => {
+    e.preventDefault();
+    const formImageData = new FormData();
+    formImageData.append('memoryImage', previewImageFile);
+
+    // Dispatch Profile image upload Action
+    dispatch(memoryImageUploadAction(memoryId, formImageData));
+    setPreviewImage('');
+  };
+
+  return (
+    <div className="memories-images-wrapper">
+      <img src={memoryImage} alt="" className="preview-image" />
+      <form onSubmit={handleImageUpdate}>
+        <InputFieldComponent
+          id="memoryImage"
+          label="Change Profile Image"
+          type="file"
+          name="memoryImage"
+          onChange={uploadFileHandler}
+        />
+
+        {previewImage ? (
+          <>
+            Image Preview
+            <img
+              src={previewImage}
+              alt="profile preview"
+              className="preview-image"
+            />
+            <button type="submit">I Like It</button>
+            <button type="button" onClick={handelCancelUpload}>
+              Cancel
+            </button>
+          </>
+        ) : null}
+      </form>
+    </div>
+  );
+};
+
+export default MemoriesImages;
--- frontend/src/store/reducers/imageUploadReducers.js
+++ frontend/src/store/reducers/imageUploadReducers.js
@@ -1,4 +1,7 @@
 import {
+  MEMORY_IMAGE_UPLOAD_FAILURE,
+  MEMORY_IMAGE_UPLOAD_REQUEST,
+  MEMORY_IMAGE_UPLOAD_SUCCESS,
   PROFILE_IMAGE_UPLOAD_FAILURE,
   PROFILE_IMAGE_UPLOAD_REQUEST,
   PROFILE_IMAGE_UPLOAD_SUCCESS,
@@ -16,6 +19,25 @@ export const profileImageUploadReducer = (state = {}, action) => {
         profileImageUploaded: action.payload,
       };
     case PROFILE_IMAGE_UPLOAD_FAILURE:
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+//Memories images
+export const memoryImageUploadReducer = (state = {}, action) => {
+  switch (action.type) {
+    case MEMORY_IMAGE_UPLOAD_REQUEST:
+      return { ...state, loading: true };
+    case MEMORY_IMAGE_UPLOAD_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        success: true,
+        memoryImageUploaded: action.payload,
+      };
+    case MEMORY_IMAGE_UPLOAD_FAILURE:
       return { ...state, loading: false, error: action.payload };
     default:
       return state;
